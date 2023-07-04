071e26b4415c2ebb20f31efeb175221fd2224eeb
Sk Md Moinul Islam <moinul0459@gmail.com>
2023-07-04 20:07:41 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const app = express();

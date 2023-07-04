ef4774b5a95ea97fd041b2fb5023f0f5161cef5b
Sk Md Moinul Islam <moinul0459@gmail.com>
2023-07-04 21:44:59 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const router = require("express").Router()
 const Todo = require("../models/Todo");
 
 // routes will be here....
-router.get("/", async(req, res) => {
-    const allTodo = await Todo.find();
-    res.render("index", {todo: allTodo})
+router.get("/", async (req, res) => {
+    const allTodo = await Todo.find().maxTimeMS(20000);
+    res.render("index", { todo: allTodo })
 })
 
 

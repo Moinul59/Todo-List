d4d93028d2c76838f645035767f3a1d7f64fd3b2
Sk Md Moinul Islam <moinul0459@gmail.com>
2023-07-04 20:44:10 +0530

    updated

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
 
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000;
 const app = express();
 
-// // Connection to MongoDB
-// mongoose.connect(process.env.DATABASE, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-mongoose
-  .connect("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Additional code
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-  });
-
+// conenction to mongodb
+mongoose.connect("mongodb://localhost/todo_list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
 // middlewares
